774f5a0a5c11f297e538b8df93a8f75112d9e080
Luca Anzaldi <anzaldi.luca@gmail.com>
2024-05-14 15:10:38 +0200

    Add TEST dir on git

---
+++ Back-End/LLVM/TEST/source_c_files/Loop1.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+
+
+
+
+
+int main(){
+	
+	int a = 3;
+	int somma = 0;
+
+	for(int i=0 ; i<10 ; i++){
+		somma = somma + a;
+	}
+
+
+	return 0;
+
+}
---
+++ Back-End/LLVM/TEST/source_c_files/Loop_invariant.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+
+int main(){
+	
+	int a = 3;
+    int b = 5;
+    int c = 1;
+
+	for(int i=0 ; i<100 ; i++){
+		a = b + c;
+	}
+
+
+	return 0;
+
+}
