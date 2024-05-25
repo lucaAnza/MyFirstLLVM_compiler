71f38fd10979ef2f58f8612766d9c48129300e2d
Luca Anzaldi <anzaldi.luca@gmail.com>
2024-05-25 16:13:08 +0200

    Add source cpp e header file on assignment4 dir

---
+++ Back-End/Esercitazioni/Assignment_4/LoopFusionPass.cpp
@@ -0,0 +1,28 @@
+#include "llvm/Transforms/Utils/LoopFusionPass.h"
+#include "llvm/Analysis/DominanceFrontier.h"
+#include "llvm/Analysis/LoopInfo.h"
+#include "llvm/Analysis/PostDominators.h"
+#include "llvm/IR/Dominators.h"
+#include "llvm/IR/InstrTypes.h"
+#include "llvm/IR/Instructions.h"
+#include <llvm/IR/Constants.h>
+
+using namespace llvm;
+
+PreservedAnalyses LoopFusionPass::run(Function &F, FunctionAnalysisManager &AM) {
+    outs() << "Start loop fusion opt...\n";
+    SmallVector<Loop*, 8> Worklist;
+
+    LoopInfo &LI = AM.getResult<LoopAnalysis>(F);
+        
+    for (Loop *TopLevelLoop : LI){
+        outs()<<"TopLevelLoop : "<<*TopLevelLoop<<"\n";
+        for (Loop *L : depth_first(TopLevelLoop)){
+            // We only handle inner-most loops.
+            if (L->isInnermost())
+                Worklist.push_back(L);
+        }
+    }
+
+    return PreservedAnalyses::all();
+}
