f1b69d2df51e90b7901ccc03b8883aefef546859
Luca Anzaldi <anzaldi.luca@gmail.com>
2024-06-12 16:33:20 +0200

    Clean up

--- Back-End/Esercitazioni/Assignment_1/create_pass.sh
+++ Back-End/Esercitazioni/Assignment_1/create_pass.sh
@@ -1,4 +1,4 @@
-NAME="LoopFusionPass"
+NAME="LocalOpts"
 
 
 
@@ -14,10 +14,11 @@ code SRC/llvm-project-llvmorg-17.0.6/llvm/lib/Transforms/Utils/$NAME.cpp
 echo "1. Add $NAME (.cpp)  in CMakeLists.txt"
 code SRC/llvm-project-llvmorg-17.0.6/llvm/lib/Transforms/Utils/CMakeLists.txt
 
-echo -e "\n2. Add LOOP_PASS(\"loop_pass\", $NAME()) in PassRegistry.def"
+echo -e "\n2. Add MODULE_PASS(\"local_opts\", $NAME()) in PassRegistry.def"
 code SRC/llvm-project-llvmorg-17.0.6/llvm/lib/Passes/PassRegistry.def
 echo -e "ATTENZIONE : al posto di "LOOP_PASS" può essere che vada messo FUNCTION_PASS,...\n"
 
 echo -e "3. Add #include \"llvm/Transforms/Utils/$NAME.h\" in PassBuilder.cpp"
 code SRC/llvm-project-llvmorg-17.0.6/llvm/lib/Passes/PassBuilder.cpp
+
 
