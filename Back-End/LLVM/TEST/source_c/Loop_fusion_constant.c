e78deaaed3413ed6c0c6f307835647cfc819f6ef
Luca Anzaldi <anzaldi.luca@gmail.com>
2024-06-26 12:47:01 +0200

    Fix Assignment4 .ll source file

--- Back-End/LLVM/TEST/source_c/Loop_fusion_constant.c
+++
@@ -1,9 +0,0 @@
-void twoloops(int a[] , int b[] , int c[] , int d[] ){
-    int i;
-
-    for(i=0 ; i<100 ; i++)
-        a[i] = b[i] + c[i];
-    
-    for(i=0 ; i<100 ; i++)
-        d[i] = a[i] * a[i];
-}
