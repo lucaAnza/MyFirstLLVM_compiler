a2deaa458df0ee236a03bb7024aad5a6782266db
Luca Anzaldi <anzaldi.luca@gmail.com>
2024-06-26 12:58:44 +0200

    Definitive clean up assignment4

--- Back-End/LLVM/TEST/source_c/Loop_fusion.c
+++ Back-End/LLVM/TEST/source_c/Loop_fusion.c
@@ -1,9 +1,9 @@
-void twoloops(int a[] , int b[] , int c[] , int d[] , int n){
+void twoloops(int a[], int b[], int n){
     int i;
+    for (i = 0; i < n; i++)
+        a[i] = i + 5;
+    for (i = 0; i < n; i++)
+        b[i] = i + i;
 
-    for(i=0 ; i<n ; i++)
-        a[i] = b[i] + c[i];
-    
-    for(i=0 ; i<n ; i++)
-        d[i] = a[i] * a[i];
+    return;
 }
